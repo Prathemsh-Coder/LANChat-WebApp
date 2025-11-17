7a8c5e339cfeddd78865e06760f15f37da11a4ba
Prathemsh-Coder <praut4236@gmail.com>
2025-11-17 19:09:58 +0530

    Updated ChatServerEndpoint logic

--- src/websocket/ChatServerEndpoint.java
+++ src/websocket/ChatServerEndpoint.java
@@ -7,38 +7,40 @@ import jakarta.websocket.server.ServerEndpoint;
 public class ChatServerEndpoint {
 
     @OnOpen
-    public void onOpen(Session session) {
-        // DO NOT ADD USER HERE
-        // Wait until real username comes from JOIN:
+    public void onOpen(Session session) 
+    {
+        
         System.out.println("New connection: " + session.hashCode());
         ChatManager.sessionMap.put(session.hashCode(), session);
     }
 
     @OnMessage
-    public void onMessage(String msg, Session session) {
+    public void onMessage(String msg, Session session)
+    {
 
         int id = session.hashCode();
 
-        // USER JOIN FLOW
+        
         if (msg.startsWith("JOIN:")) {
             String uname = msg.substring(5);
 
-            // Remove old fake "User-id" entries if they existed
+           
             ChatManager.userList.removeUser(id);
 
-            // Add real user
+            
             ChatManager.userList.addUser(uname, id);
 
             ChatManager.broadcast("🟢 " + uname + " joined the chat.");
             return;
         }
 
-        // NORMAL CHAT MESSAGE
+        
         ChatManager.broadcast(msg);
     }
 
     @OnClose
-    public void onClose(Session session) {
+    public void onClose(Session session)
+    {
         int id = session.hashCode();
         String username = ChatManager.userList.getUsernameById(id);
 
